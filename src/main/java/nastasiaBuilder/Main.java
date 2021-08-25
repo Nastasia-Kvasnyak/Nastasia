082d09ce5b64e3604ddf038181185578be4860db
Anastasiya Kvasnyak <nastya.odagiri@gmail.com>
2021-08-25 17:39:51 +0300

    lab with Builder (client)

---
+++ src/main/java/nastasiaBuilder/Main.java
@@ -0,0 +1,16 @@
+package nastasiaBuilder;
+/*
+* builder for client
+* */
+
+public class Main {
+    public static void main(String[] args) {
+       Client client = Client.builder()
+               .name("Nastya")
+               .salary(77)
+               .debt(10)
+               .bonus(5)
+               .build();
+        System.out.println(client);
+    }
+}
