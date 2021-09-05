aeccbc4f5041fcdb1070e3df4010ede67d4f6111
Anastasiya Kvasnyak <nastya.odagiri@gmail.com>
2021-09-06 01:14:35 +0300

    radio alarm with encapsulation, modified

---
+++ src/main/java/homework/radio_alarm/RadioImpl2.java
@@ -0,0 +1,13 @@
+package homework.radio_alarm;
+
+public class RadioImpl2 implements Radio {
+    @Override
+    public void a() {
+        System.out.println("I am new method a");
+    }
+
+    @Override
+    public void b() {
+        System.out.println("I am new method b");
+    }
+}
